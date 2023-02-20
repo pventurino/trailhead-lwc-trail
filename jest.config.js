fa329e04f3c5dfe178dee6976b24ed0089d9309e
Pablo Venturino <pablojventurino@gmail.com>
2023-02-20 10:51:43 -0800

    mock other components: thunder/hammer-button
    https://trailhead.salesforce.com/content/learn/modules/test-lightning-web-components/mock-other-components?trail_id=build-lightning-web-components

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,8 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 module.exports = {
     ...jestConfig,
     moduleNameMapper: {
-        '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button'
+        '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button',
+        '^thunder/hammerButton$': '<rootDir>/force-app/test/jest-mocks/thunder/hammerButton'
     },
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
 };
