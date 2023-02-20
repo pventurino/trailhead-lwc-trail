9da8e79bfc50b51ca47cb88c4fcea84bb7fef16a
Pablo Venturino <pablojventurino@gmail.com>
2023-02-20 11:00:20 -0800

    mock other components; c/displayPanel
    There will be times when the LWC you are working with imports another LWC that is not in the local LWC directory of your Salesforce DX project. Without a stub they will cause Jest to fail.
    
    displayPanel.js: Notice that there is an api decorator for each parameter passed in the component call.
    
    https://trailhead.salesforce.com/content/learn/modules/test-lightning-web-components/mock-other-components?trail_id=build-lightning-web-components

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 module.exports = {
     ...jestConfig,
     moduleNameMapper: {
+        '^c/displayPanel$': '<rootDir>/force-app/test/jest-mocks/c/displayPanel',
         '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button',
         '^thunder/hammerButton$': '<rootDir>/force-app/test/jest-mocks/thunder/hammerButton'
     },
